1dc549ce2c4a93613f7cd422ddd2653734723165
Demidoff Serg <serega.dem@inbox.ru>
2023-04-11 11:20:59 +0800

    add java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int balance = 500;
         int amount = 1500;
-        int percent=100;
+        int percent = 100;
         if (amount > 1000) {
-            System.out.println("Бонус"+(balance+amount)/percent);
+            System.out.println("Бонус" + (balance + amount) / percent);
         } else {
             System.out.println("Бонус равен 0");
         }
