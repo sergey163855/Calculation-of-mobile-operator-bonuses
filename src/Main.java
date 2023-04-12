3fba5b5e1caa1ade5c5f058b3a0dd1a0c52834f4
Demidoff Serg <serega.dem@inbox.ru>
2023-04-12 15:17:37 +0800

    add balance

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         int balance = 500;
-        int amount = 1500;
+        int amount = 2000;
         int percent = 100;
         if (amount > 1000) {
             System.out.println("Бонус" + (balance + amount) / percent);
+            System.out.println("Баланс" + (balance + amount));
         } else {
-            System.out.println("Бонус равен 0");
+            System.out.println("Бонус 0");
+            System.out.println("Баланс" + (balance + amount));
         }
     }
 }
