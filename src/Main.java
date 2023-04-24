ca57da289ec91cf55044a15029349e1ba9264f9f
Demidoff Serg <serega.dem@inbox.ru>
2023-04-24 15:21:38 +0800

    add changes3

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,16 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int balance = 100;
+        int startingBalance = 100;
         int amount = 1100;
-        int percent =100;
+
+        int bonus = 0;
         if (amount > 1000) {
-            System.out.println("Бонус" + amount / percent );
-            System.out.println("Итоговый баланс" + ((amount / percent) + amount + balance));
-        } else {
-            System.out.println("Бонус 0");
-            System.out.println("Итоговый баланс"+(balance + amount));
+            bonus = amount / 100;
+        }
+        int endingBalance = startingBalance + amount +bonus;
+        System.out.println("Итоговый баланс:" + endingBalance);
+        System.out.println("бонус:" + bonus);
         }
     }
-}
+
